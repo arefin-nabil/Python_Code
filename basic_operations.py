f94723686b039483cd07a373d9ef1eb3b8d89dfd
Nurul Arefin Nabil <arefinnabil256@gmail.com>
2025-10-01 15:54:46 +0600

    Basic Python

---
+++ basic_operations.py
@@ -0,0 +1,29 @@
+# Python Arithmetic Operation
+a=15
+b=2
+
+print(a+b)   # + Addition                   # 17
+print(a-b)   # - Subtraction                # 13
+print(a*b)   # * Multiplication             # 30
+print(a/b)   # / Division                   # 7.5 --- (float value)
+print(a//b)  # // Floor division            # 7 --- (integer value)
+print(a%b)   # % Modulus                    # 1 --- (mod/remainder)
+print(2**3)  # ** Exponentiation            # 8 --- (power 2^3)
+
+
+
+
+# Boolean value
+# returns only True or False
+
+
+
+# Python Comparison Operator
+print(10==5)    # == Equal to                   # False
+print(10!=5)    # != Not Equal                  # True
+print(10>5)     # > Greater than                # True
+print(10<5)     # < Less than                   # False
+print(10>=10)   # == Greater than or Equal      # True
+print(10<=10)   # == Less than or Equal         # True
+
+
